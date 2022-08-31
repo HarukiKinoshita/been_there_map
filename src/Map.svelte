c1a07b317964618f71357e54d4fd22dca1c8ecf4
HarukiKinoshita <harukikinoshita@outlook.com>
2022-08-31 09:12:35 +0200

    Fix: Split Canarias from others

--- src/Map.svelte
+++ src/Map.svelte
@@ -10,7 +10,8 @@
 
   // const projection = geoNaturalEarth1().scale(2000).translate([460,1900]);
   const projection = d3_composite.geoConicConformalSpain();
-  const path = geoPath().projection(projection);
+  const default_path = geoPath().projection(projection.translate([0, 0]));
+  const translated_path = geoPath().projection(projection.translate([0, 200]));
 
   $: themeColor = "#ff3e00";
 
@@ -98,13 +99,13 @@
   <span class="headline">{ Object.keys(visited_list).length }</span><span style="color: slategray; margin-left: 4px; ">/ { mode.length }</span><br>
 </p>
 <div id="map_container">
-  <svg viewBox="300 0 500 500" preserveAspectRatio="xMidYMid meet">
+  <svg viewBox="-200 200 500 500" preserveAspectRatio="xMidYMid meet" style="border: 1px solid black;">
   <!-- <svg viewBox="0 0 960 500" preserveAspectRatio="xMidYMid meet"> -->
     <g>
-      {#each mode as feature, i}
+      {#each mode.filter(el => el.properties.name !== "Canarias") as feature, i}
         <path
           id={feature.properties.name}
-          d={path(feature)}
+          d={default_path(feature)}
           class="area"
           fill={visited_list[feature.properties.name] ? themeColor : "#fff"}
           on:mouseover={() => {hovered = feature, showTooltip(feature.properties.name)}}
@@ -113,6 +114,11 @@
           on:click={() => {addToList(feature.properties)}} 
         />
       {/each}
+      <path
+        d={translated_path(mode.find(el => el.properties.name == "Canarias"))}
+        class="area"
+        fill={"blue"}
+      />
     </g>
   </svg>
 </div>
