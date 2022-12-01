e5b6f8fb3e0263aea0c3e19194aaf9e7ee7c2880
HarukiKinoshita <harukikinoshita@outlook.com>
2022-12-01 08:37:06 +0100

    Fix content

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,11 +1,22 @@
+<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="h13a" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+
 <div id="wrapper">
   <h1 class="headline">Been There Map</h1>
-  <p>Select the map and get started!</p>
   <div class="menu">
-    <a href="/#/europe">🇪🇺 Europe</a>
+    <p>Select the map you create and get started!</p>
+    <a class="button" href="/#/europe">🇪🇺 Europe</a>
 		<a href="/#/spain">🇪🇸 Spain</a>
 		<a href="/#/france">🇫🇷 France</a>
 		<a href="/#/germany">🇩🇪 Germany</a>
+  </div>
+  <div id="about">
+    <p>
+      <strong>Been There Map</strong>
+      by Haruki Kinoshita
+    </p>
+    <a href="https://www.buymeacoffee.com/h13a" target="_blank">
+      <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 40px !important;width: 142px !important;" >
+    </a>
   </div>
 </div>
 
@@ -14,6 +25,9 @@
     padding: 1em;
     background-color: hsl(0, 0%, 95%);
   }
+  #about {
+    margin-top: 12vh;
+  }
   .headline {
 		color: #ff3e00;
 		text-transform: uppercase;
@@ -21,9 +35,14 @@
 		font-weight: 100;
 	}
   .menu {
-    font-size: 2em;
+    background-color: #ffffff;
+    padding: 1em 1em 2em 1em;
+    border-radius: 20px;
+    margin: auto;
+    max-width: 840px;
   }
   .menu a {
-    padding: 1vw;
+    font-size: 1.5em;
+    padding: 0 1vw;
   }
 </style>
