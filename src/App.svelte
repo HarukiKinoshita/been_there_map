e05b6f8072b0122d69609543ff6c42c02cb328c5
HarukiKinoshita <harukikinoshita@outlook.com>
2022-12-02 17:16:56 +0100

    Fix: metadata

--- src/App.svelte
+++ src/App.svelte
@@ -59,19 +59,3 @@
 		display: inline-block;
 	}
 </style>
-
-<svelte:head>
-	<title>Been There Map</title>
-	<html lang="en" />
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Where have you visited? Create your own Been There map and share it with your friends.">
-	<meta property="og:url" content="https://been-there-map.h13a.dev/" />
-	<meta property="og:title" content="Been There Map" />
-	<meta property="og:type" content="website" />
-	<meta property="og:description" content="Where have you visited? Create your own Been There map and share it with your friends." />
-	<meta property="og:site_name" content="Been There Map" />
-	<meta property="og:image" content="https://been-there-map.h13a.dev/been-there-map-logo.png" />
-	<meta name="twitter:card" content="summary" />
-	<meta name="twitter:site" content="https://been-there-map.h13a.dev/">
-</svelte:head>
